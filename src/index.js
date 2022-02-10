5108a46f8471f38b27165a0aa1d2ef73d9349bf7
BibinJP <97421150+BibinJP@users.noreply.github.com>
2022-02-10 15:40:41 +0530

    first season

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDispaly from "./seasonDisplay";
+import Spinner from "./Spinner";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// const App = () => {
+//   window.navigator.geolocation.getCurrentPosition(
+//     (position) => console.log(position),
+//     (err) => console.log(err)
+//   );
+class App extends React.Component {
+  //   constructor(props) {
+  //     super(props);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  //     this.state = { lat: null, errorMessaage: "" };
+  //   }
+
+  state = { lat: null, errorMessaage: "" };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => this.setState({ errorMessaage: err.message })
+    );
+  }
+  componentDidUpdate() {
+    console.log("My cop did update");
+  }
+  render() {
+    if (this.state.errorMessaage && !this.state.lat) {
+      return <div>Error:{this.state.errorMessaage}</div>;
+    }
+    if (!this.state.errorMessaage && this.state.lat) {
+      return <SeasonDispaly lat={this.state.lat} />;
+    }
+    return <Spinner message=" Please provide location request" />;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
